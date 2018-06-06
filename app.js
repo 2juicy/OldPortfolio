0b4282780735526e37f82841bc1288f1dc839458
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-05 18:37:50 -0700

    copy paste start

---
+++ app.js
@@ -0,0 +1,73 @@
+// All ends must have beginnings...
+$(document).ready(function(){
+//Lets give the 1 page setup a try with some flair...
+// $('#aboutmepage').hide();
+// $('#portfoliopage').hide();
+
+
+
+
+
+// TypeIt
+var instance = new TypeIt('.taglineHead', {
+    strings: ['Node.js Developer...', 'Video Game Enthusiast...'],
+    nextStringDelay: 2000,
+    cursor: false,
+    breakLines: false,
+    loop: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // The End...
