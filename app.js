0ee46d7684a96d8f5ac4b723122a2020709a8bb4
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-05 20:44:16 -0700

    updated profile done

--- app.js
+++ app.js
@@ -2,41 +2,18 @@
 $(document).ready(function(){
 // $('#aboutmepage').hide();
 $('#portfoliopage').hide();
-
+// Buttons hiding and showing pages.
 $("#aboutClick").on("click", function(event) {
     $('#aboutmepage').slideDown();
     $('#portfoliopage').slideUp();
 });
-
-
-
-
-
 $("#portfolioClick").on("click", function(event) {
     $('#aboutmepage').slideUp();
     $('#portfoliopage').slideDown();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // TypeIt
 var instance = new TypeIt('.taglineHead', {
-    strings: ['Node.js Developer...', 'Video Game Enthusiast...'],
+    strings: ['Freelance Web Developer...', 'Living in Node.js...'],
     nextStringDelay: 2000,
     cursor: false,
     breakLines: false,
